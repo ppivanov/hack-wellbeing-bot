1e61df70c31f9ed92724a4f5332328aef24d0b39
Dennis Dimov <dennisdimov@yahoo.de>
2021-09-30 11:42:25 +0200

    token adjustments

--- backend/healthbot.py
+++ backend/healthbot.py
@@ -6,5 +6,6 @@ from dotenv import load_dotenv
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
-client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+client = slack.WebClient('xoxb-2535729735287-2574128624896-GL5JKsUH9q6u7exZSGrtJibT')
+client.chat_postMessage(channel='#botter', text='Hello!')
 
