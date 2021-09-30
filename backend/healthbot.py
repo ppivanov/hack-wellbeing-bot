028dda9e2df7a95e1a0858424c36a40f0e09fe86
Josipo <josefwsvec@gmail.com>
2021-09-30 14:16:24 +0200

    First commit on my good looking Mac

--- backend/healthbot.py
+++ backend/healthbot.py
@@ -2,10 +2,11 @@ import slack
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import ssl
 
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
+ssl._create_default_https_context = ssl._create_unverified_context
 client = slack.WebClient('xoxb-2535729735287-2574128624896-GL5JKsUH9q6u7exZSGrtJibT')
-client.chat_postMessage(channel='#botter', text='Hello!')
-
+client.chat_postMessage(channel='#botter', text='Hello from Josef!')
