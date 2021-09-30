46baca9dff0d5e1e3e14dd765c1ac9d39e66bf35
Martin Korytak <martin.korytak@workday.com>
2021-09-30 13:08:53 +0200

    added private messages to each of us

--- backend/healthbot.py
+++ backend/healthbot.py
@@ -1,11 +1,36 @@
 import slack
-import os
+import json
+import random
+import time
+import emoji
 from pathlib import Path
 from dotenv import load_dotenv
 
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
-client = slack.WebClient('xoxb-2535729735287-2574128624896-GL5JKsUH9q6u7exZSGrtJibT')
-client.chat_postMessage(channel='#botter', text='Hello!')
+with open('data/jokes.json') as f:
+    jokes = json.load(f)
 
+joke = random.choice(jokes)
+question = joke['question']
+answer = joke['answer']
+
+client = slack.WebClient('xoxb-2535729735287-2574128624896-GL5JKsUH9q6u7exZSGrtJibT')
+
+users = {
+    "Martin Korytak": "U02GW2LF472",
+    "Tom Callaghan": "U02G6DM24KU",
+    "Pavel Ivanov": "U02FZM0QG3Y",
+    "Dennis Dimov": "U02GW22S76U",
+    #"Josef Svec": ""
+}
+
+for i in users.values():
+    response = client.conversations_open(users=[i])
+    channel = response.data['channel']['id']
+
+    client.chat_postMessage(channel=channel, text=question)
+    time.sleep(10)
+    client.chat_postMessage(channel=channel, text=answer)
+    client.chat_postMessage(channel=channel, text=emoji.emojize(':rolling_on_the_floor_laughing:'))
