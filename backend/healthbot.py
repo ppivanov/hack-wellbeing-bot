9151de2d143ac18ec1596290f2ad3274d9007315
Tom Callaghan <tom.callaghan4@mail.dcu.ie>
2021-09-30 11:04:36 +0100

    fixed ssl issue

--- backend/healthbot.py
+++ backend/healthbot.py
@@ -2,6 +2,8 @@ import slack
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
