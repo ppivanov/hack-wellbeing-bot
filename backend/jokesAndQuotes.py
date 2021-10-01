fc9dc2d36c31119da9c1a3d390594905490c07ae
Josipo <josefwsvec@gmail.com>
2021-10-01 11:30:41 +0200

    Should be working now

--- backend/jokesAndQuotes.py
+++ backend/jokesAndQuotes.py
@@ -5,56 +5,15 @@ import time
 import emoji
 import ssl
 import utils
+
 ssl._create_default_https_context = ssl._create_unverified_context
-
-
 
 # env_path = Path('.') / '.env'
 # load_dotenv(dotenv_path=env_path)
-def JokesAndQuotes(username_routine):
 
-    # client = slack.WebClient('xoxb-2535729735287-2574128624896-GL5JKsUH9q6u7exZSGrtJibT')
-    # response = client.conversations_open(users= "U02G6DGQ0LA") #Do not forgot to authomate this
-    # channel = response.data['channel']['id']
-    ts = utils.send_message(
-        username = username_routine,
-        text = "Hello!"
-    )
-    def Choosing():
-        utils.send_message(
-            username=username_routine, 
-            text="Do you fell like a quote or a joke today? " \
-                f"React :dart: or :rolling_on_the_floor_laughing:"
-            )
-        emoji = utils.get_reactions(username = username_routine, 
-                                    timestamps=[ts],
-                                    discipline="")
+def JokesAndQuotes(username_indv):
 
-        #this is a temporary solution
-        # if emoji == ':dart:':
-        #     Quotes()
-        # elif emoji == ':rolling_on_the_floor_laughing:':
-        #     Jokes()
-        # else:
-        #     send("Sorry, you did not indicate your choice clearly." \
-        #         "\n Let me ask you again.")
-    def react_jokes_quotes(username, timestamp):
-        channel = utils.resolve_user(username)
-        allowed_reactions = utils.get_allowed_reactions('jokesQuotes')
-
-        response = client.reactions_get(channel=channel, timestamp=timestamp)
-        reactions = utils.get_all_reactions(response)
-
-        utils.check_reaction(username, reactions, allowed_reactions)
-
-        reaction = reactions.pop()
-        if reaction == ':dart:':
-            Quotes()
-        elif reaction == ':rolling_on_the_floor_laughing:':
-            Jokes()         
-
-
-    def Jokes():
+    def Jokes(username_routine):
         with open('../data/jokes.json') as f:
             jokes = json.load(f)
 
@@ -62,14 +21,10 @@ def JokesAndQuotes(username_routine):
         question = joke['question']
         answer = joke['answer']
 
-        for i in users.values():
-            response = client.conversations_open(users=[i])
-            channel = response.data['channel']['id']
-
-            client.chat_postMessage(channel=channel, text="Question: {}".format(question))
-            time.sleep(2)
-            client.chat_postMessage(channel=channel, text="Answer: {}".format(answer))
-            client.chat_postMessage(channel=channel, text=emoji.emojize(':rolling_on_the_floor_laughing:'))
+        utils.send_message(username = username_routine, text = f"Question: {question}" )
+        time.sleep(2)
+        utils.send_message(username= username_routine, text = f"Answer: {answer}")
+        utils.send_message(username= username_routine, text=":rolling_on_the_floor_laughing:")
 
     def Quotes(username_routine):
 
@@ -80,18 +35,37 @@ def JokesAndQuotes(username_routine):
         quote = quote_slip['text']
         author = quote_slip['author']
 
-        # client = slack.WebClient('xoxb-2535729735287-2574128624896-GL5JKsUH9q6u7exZSGrtJibT')
-        response = client.conversations_open(users= "U02G6DGQ0LA") #Do not forgot to authomate this
-        channel = response.data['channel']['id']
-
         utils.send_message(
-            username = username_routine,
-            text=f"Quote to get you through the day: \"{quote}\" \n by: {author}"
-            )
+                username = username_routine,
+                text=f"Quote to get you through the day: \"{quote}\" \n by: {author}"
+                )
+
+    def react_jokes_quotes(username, timestamp):
+        channel = utils.resolve_user(username)
+        allowed_reactions = utils.get_allowed_reactions('jokesQuotes')
+
+        response = utils.client.reactions_get(channel=channel, timestamp=timestamp)
+        reactions = utils.get_all_reactions(response)
+
+        utils.check_reaction(username, reactions, allowed_reactions)
+
+        reaction = reactions.pop()
+        if reaction == 'dart':
+            Quotes(username_indv)
+        elif reaction == 'rolling_on_the_floor_laughing':
+            Jokes(username_indv)
+    
+    ts = utils.send_message(
+    username = username_indv,
+    text = "Hello!, Do you fell like a quote or a joke today? " \
+                f"React :dart: or :rolling_on_the_floor_laughing:"
+        )
+
+    react_jokes_quotes(username = username_indv,
+                        timestamp = ts)
+ 
 
 
 
-# JokesAndQuotes()
-
-for k in users:
-    print(k)
+# for user in utils.users:
+#     JokesAndQuotes(username_routine= user,)
